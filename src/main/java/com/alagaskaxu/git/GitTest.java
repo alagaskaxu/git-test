7fd437fc6d4141ded4f3645a8b5b7166e88cf66a
徐念 <11272008+alagaskaxu@user.noreply.gitee.com>
2022-07-06 16:57:46 +0000

    gitee test

--- src/main/java/com/alagaskaxu/git/GitTest.java
+++ src/main/java/com/alagaskaxu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("main hot-fix conflict merge");//冲突合并test
         System.out.println("pushtest");
         System.out.println("pulltest");
+        System.out.println("giteetest");
     }
 }
 //这里注意一下：IDEA 实现版本切换回看的功能，并不是使用的 reset
